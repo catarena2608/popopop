4eb51d1715eedf2ba5598249bcc4460254186025
catarena2608 <thodeptrai11111@gmail.com>
2024-10-03 13:23:20 +0700

    tienancut

---
+++ calculator reall/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+namespace calculator_reall
+{
+    public partial class Form1 : Form
+    {
+        float data1, data2;
+        string pheptinh;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 0)
+            {
+                textBox1.Text = textBox1.Text.Substring(0, textBox1.Text.Length - 1);
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            pheptinh = "chia";
+            data1 = float.Parse(textBox1.Text);
+            textBox1.Clear();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "9";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "0";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            pheptinh = "cong";
+            data1 = float.Parse(textBox1.Text);
+            textBox1.Clear();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            pheptinh = "tru";
+            data1 = float.Parse(textBox1.Text);
+            textBox1.Clear();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            pheptinh = "nhan";
+            data1 = float.Parse(textBox1.Text);
+            textBox1.Clear();
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            if (pheptinh == "cong")
+            {
+                data2 = data1 + float.Parse(textBox1.Text);
+                textBox1.Text = data2.ToString();
+            }
+            if (pheptinh == "tru")
+            {
+                data2 = data1 - float.Parse(textBox1.Text);
+                textBox1.Text = data2.ToString();
+            }
+            if (pheptinh == "nhan")
+            {
+                data2 = data1 * float.Parse(textBox1.Text);
+                textBox1.Text = data2.ToString();
+            }
+            if (pheptinh == "chia")
+            {
+                if (float.Parse(textBox1.Text) == 0)
+                {
+                    MessageBox.Show("không thể chia cho 0");
+                    textBox1.Clear();
+                }
+                else
+                {
+                    data2 = data1 / float.Parse(textBox1.Text);
+                    textBox1.Text = data2.ToString();
+                }
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + ".";
+        }
+    }
+}
